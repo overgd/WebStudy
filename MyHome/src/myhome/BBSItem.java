78c922e2d34ed26b495c6e4458720cabdc70dd86
Administrator <Administrator@Sc-201504131508>
2015-06-05 18:14:05 +0900

    150605

---
+++ MyHome/src/myhome/BBSItem.java
@@ -0,0 +1,37 @@
+package myhome;
+
+public class BBSItem {
+	
+	private int seqNo;
+	private String title;
+	private String writer;
+	private String content;
+	
+	public int getSeqNo() {
+		return seqNo;
+	}
+	public void setSeqNo(int seqNo) {
+		this.seqNo = seqNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
